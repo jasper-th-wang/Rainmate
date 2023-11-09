ccf165bcfdf96c96dedc247365ecd0186d40cc87
Jasper Wang <jasper8777@icloud.com>
2023-11-09 09:22:14 -0800

    Implement Available umbrellas, fix read more btn

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -1,6 +1,6 @@
 // Function to store reservation data in Firebase
 let params = new URL(window.location.href); //get URL of search bar
-let vendorID = params.searchParams.get("id"); //get value for key "id"
+let vendorID = params.searchParams.get('id'); //get value for key "id"
 console.log(vendorID);
 
 function handleFormSubmit(event) {
@@ -9,7 +9,7 @@ function handleFormSubmit(event) {
   // const vendorName = document.getElementById('vendorName').value;
 
   // Store the reservation data in Firebase
-  db.collection("Reservations")
+  db.collection('Reservations')
     .add({
       user_id: firebase.auth().currentUser.uid,
       vendor_id: vendorID,
@@ -18,15 +18,15 @@ function handleFormSubmit(event) {
     })
     .then((reservation) => {
       // console.log(test.id);
-      console.log("Reservation added to Firebase");
+      console.log('Reservation added to Firebase');
       window.location.href = `../confirmation.html?id=${reservation.id}`;
     })
     .catch((error) => {
-      console.error("Error adding reservation:", error);
+      console.error('Error adding reservation:', error);
     });
 }
 
 // Attach event listener to the form
 document
-  .getElementById("reservationForm")
-  .addEventListener("submit", handleFormSubmit);
+  .getElementById('reservationForm')
+  .addEventListener('submit', handleFormSubmit);
