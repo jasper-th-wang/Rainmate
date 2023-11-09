e2e38d7e131b88998c9879b58c25f6f1748f0b2b
Jasper Wang <jasper8777@icloud.com>
2023-11-09 10:30:38 -0800

    add current reservation id to user in reservation.js

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -3,8 +3,24 @@ let params = new URL(window.location.href); //get URL of search bar
 let vendorID = params.searchParams.get('id'); //get value for key "id"
 console.log(vendorID);
 
+function registerReservationIDToUser(reservationID) {
+  // Check if the user is logged in:
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+      // currentUserUID = db.collection('users').doc(user.uid); // Go to the Firestore document of the user
+      return db.collection('users').doc(user.uid).update({
+        current_reservation: reservationID,
+      });
+    } else {
+      console.log('No user is logged in.'); // Log a message when no user is logged in
+    }
+  });
+}
+
 function handleFormSubmit(event) {
   event.preventDefault();
+  let reservationID;
 
   // const vendorName = document.getElementById('vendorName').value;
 
@@ -18,8 +34,13 @@ function handleFormSubmit(event) {
     })
     .then((reservation) => {
       // console.log(test.id);
+      reservationID = reservation.id;
       console.log('Reservation added to Firebase');
-      window.location.href = `../confirmation.html?id=${reservation.id}`;
+      return registerReservationIDToUser(reservationID);
+    })
+    .then((user) => {
+      console.log(user);
+      window.location.href = `../confirmation.html?id=${reservationID}`;
     })
     .catch((error) => {
       console.error('Error adding reservation:', error);
