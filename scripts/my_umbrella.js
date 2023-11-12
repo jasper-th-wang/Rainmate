a3db38be81b281333e9c210e8ef2e9c555a99585
Jasper Wang <jasper8777@icloud.com>
2023-11-12 13:15:35 -0800

    initial implementation for my_umbrella functions, not working currently

--- scripts/my_umbrella.js
+++ scripts/my_umbrella.js
@@ -14,20 +14,20 @@
 //     }
 //   });
 // }
+const pickupBtn = document.getElementById('pickupTestBtn');
 
-function addPickUpTimeToReservation(reservationID) {
-  const reservation = db.collection('reservations').doc(reservationID);
+function addPickUpTimeToReservation(reservation) {
+  console.log(reservation);
   reservation.update({
     pickup: firebase.firestore.FieldValue.serverTimestamp(),
   });
-  return reservation.vendor_id;
 }
-function decrementVendorUmbrellaCount(vendorID) {
-  const vendor = db.collection('vendors').doc(vendorID);
-  // let vendorUmbrellaCount;
-  vendor
+
+function decrementVendorUmbrellaCount(vendor) {
+  return vendor
     .get()
     .then((doc) => {
+      console.log('vendor ID is ' + doc.id);
       return doc.available_umbrellas;
     })
     .then((count) => {
@@ -36,11 +36,27 @@ function decrementVendorUmbrellaCount(vendorID) {
       });
     });
 }
-function pickUpBtnHandler(reservationID, vendorID) {
-  addPickUpTimeToReservation(reservationID);
-  decrementVendorUmbrellaCount(vendorID);
+function pickUpBtnHandler(reservationDoc) {
+  // addPickUpTimeToReservation(reservationDoc);
+
+  reservationDoc.update({
+    pickup: firebase.firestore.FieldValue.serverTimestamp(),
+  });
+
+  reservationDoc
+    .get()
+    .then((item) => {
+      console.log(item.vendor_id);
+      let reservedFromVendor = db.collection('vendors').doc(item.vendor_id);
+      return reservedFromVendor.get();
+    })
+    .then((vendor) => {
+      console.log(vendor.name);
+    });
+  console.log('successfully picked up umbrella??');
 }
 function myUmbrellaPageController() {
+  let currentReservationID;
   // Check if the user is logged in:
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
@@ -49,12 +65,19 @@ function myUmbrellaPageController() {
       currentUser.get().then((userDoc) => {
         // Get the user name
         let userName = userDoc.data().name;
-        let currentReservationID = userDoc.data().current_reservation || null;
+        currentReservationID = userDoc.data().current_reservation || null;
+        console.log(currentReservationID, ' is the current reservation ID');
+        const reservation = db
+          .collection('Reservations')
+          .doc(currentReservationID);
+        // console.log(reservation);
+        pickupBtn.addEventListener('click', () => {
+          console.log(currentReservationID);
+          pickUpBtnHandler(reservation);
+        });
         console.log(userName);
         //$("#name-goes-here").text(userName); // jQuery
         document.getElementById('user_name').innerText = userName;
-
-        // addPickUpTimeToReservation(currentReservationID);
       });
     } else {
       console.log('No user is logged in.'); // Log a message when no user is logged in
