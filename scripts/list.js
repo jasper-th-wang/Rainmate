53ecbb7d96e5f4a3624ea36ed9a8e1a9fe62ec35
JoeyChocolate <echo35@my.bcit.ca>
2023-11-14 14:48:21 -0800

    Add the function to calculate the distance

--- scripts/list.js
+++ scripts/list.js
@@ -1,3 +1,19 @@
+
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    var dLat = toRad(lat2 - lat1);
+    var dLon = toRad(lon2 - lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+}
+
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
@@ -19,10 +35,10 @@ function displayCardsDynamically(collection) {
                 newCard.querySelector('.card-image').src = `./images/vendors/${vendorCode}.png`; //Example: NV01.jpg
                 newCard.querySelector('.card-distance').innerHTML = vendorDistance + "km";
 
-                document.addEventListener("click", function(event){
+                document.addEventListener("click", function (event) {
                     target = event.target.closest(".card");
 
-                    if (target){
+                    if (target) {
                         window.location.href = `./vendor.html?id=${docID}`;
                     }
                 })
