39a66ee7050e80b12046baccd1feed3b445db6a3
Jasper Wang <jasper8777@icloud.com>
2023-11-15 14:08:44 -0800

    migrate a few functions to script.js from map.js

--- scripts/map.js
+++ scripts/map.js
@@ -12,45 +12,6 @@ function dayIndexToStr(dayIndex) {
 }
 
 // Converts numeric degrees to radians
-function toRad(Value) {
-  return (Value * Math.PI) / 180;
-}
-function calculateDistance(userCoordinates, vendorCoordinates) {
-  var [lng1, lat1] = userCoordinates;
-  var [lng2, lat2] = vendorCoordinates;
-
-  var R = 6371; // km
-  var dLat = toRad(lat2 - lat1);
-  var dLon = toRad(lng2 - lng1);
-  var lat1 = toRad(lat1);
-  var lat2 = toRad(lat2);
-
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return d;
-}
-
-function assignDistancesToVendor(userCoordinates) {
-  // calculateDistance
-  for (let i = 0; i < sessionStorage.length; i++) {
-    const itemKey = sessionStorage.key(i);
-    if (itemKey.includes('vendor-')) {
-      console.log(itemKey);
-      const itemData = JSON.parse(sessionStorage.getItem(itemKey));
-      console.log(itemData);
-      console.log(itemData.coordinates);
-      console.log(calculateDistance(userCoordinates, itemData.coordinates));
-      itemData.distance = calculateDistance(
-        userCoordinates,
-        itemData.coordinates
-      );
-      sessionStorage.setItem(itemKey, JSON.stringify(itemData));
-    }
-  }
-}
 
 function showMap() {
   //-----------------------------------------
@@ -120,6 +81,7 @@ function showMap() {
                 sessionStorage.setItem(
                   `vendor-${doc.id}`,
                   JSON.stringify({
+                    ...doc.data(),
                     coordinates: coordinates,
                     distance: 0,
                   })
