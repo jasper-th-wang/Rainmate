e4f0610d225683603a6478e45c3f9d9dce2b7da5
Jasper Wang <jasper8777@icloud.com>
2023-11-27 14:56:02 -0800

    reformat code

--- scripts/my_umbrella.js
+++ scripts/my_umbrella.js
@@ -1,40 +1,38 @@
-
-const pickUpBtn = document.getElementById('pickUpTestBtn');
-const returnBtn = document.getElementById('returnTestBtn');
+const pickUpBtn = document.getElementById("pickUpTestBtn");
+const returnBtn = document.getElementById("returnTestBtn");
 const pendingPickUpMessage =
   '<h1 class="header-message">Time Remaining for Pick Up:</h1>';
 const pendingReturnMessage =
   '<h1 class="header-message">Time Remaining for Return:</h1>';
-const VendorCardTemplate = document.getElementById('my-umbrella-card');
+const VendorCardTemplate = document.getElementById("my-umbrella-card");
 
 async function renderVendorCard(vendorID, isPickedUp) {
   // Get vendor information
-  let vendorDoc = await db.collection('vendors').doc(vendorID).get();
+  let vendorDoc = await db.collection("vendors").doc(vendorID).get();
   let vendorData = vendorDoc.data();
   // Render Vendor Card
   const vendorCard = VendorCardTemplate.content.cloneNode(true);
   document
-    .querySelector('.hi-user-name')
+    .querySelector(".hi-user-name")
     .insertAdjacentHTML(
-      'afterend',
-      isPickedUp ? pendingReturnMessage : pendingPickUpMessage
+      "afterend",
+      isPickedUp ? pendingReturnMessage : pendingPickUpMessage,
     );
 
-  vendorCard.querySelector('#card-title').innerHTML = isPickedUp
-    ? 'Pending Return'
-    : 'Pending Pick Up';
-  vendorCard.querySelector('#card-vendor-name').innerHTML = vendorData.name;
-  vendorCard.querySelector('#card-vendor-address').innerHTML = vendorData.address;
-  vendorCard.querySelector('#card-vendor-link').innerHTML =
+  vendorCard.querySelector("#card-title").innerHTML = isPickedUp
+    ? "Pending Return"
+    : "Pending Pick Up";
+  vendorCard.querySelector("#card-vendor-name").innerHTML = vendorData.name;
+  vendorCard.querySelector("#card-vendor-address").innerHTML =
+    vendorData.address;
+  vendorCard.querySelector("#card-vendor-link").innerHTML =
     `Find store<div class="material-symbols-outlined">location_on</div>`;
-  vendorCard.querySelector(
-    '#card-vendor-link'
-  ).href = `http://maps.google.com?q=${vendorData.lat}, ${vendorData.lng}`;
-  vendorCard.querySelector(
-    '#card-vendor-img'
-  ).src = `./images/vendors/${vendorData.code}.png`;
+  vendorCard.querySelector("#card-vendor-link").href =
+    `http://maps.google.com?q=${vendorData.lat}, ${vendorData.lng}`;
+  vendorCard.querySelector("#card-vendor-img").src =
+    `./images/vendors/${vendorData.code}.png`;
 
-  document.querySelector('.content-container').appendChild(vendorCard);
+  document.querySelector(".content-container").appendChild(vendorCard);
 
   renderModal();
 }
@@ -53,7 +51,7 @@ async function handleReturn(currentUser, currentReservation) {
   });
   // increment umbrella count to vendor
   const incrementUmbrellaCount = db
-    .collection('vendors')
+    .collection("vendors")
     .doc(vendorId)
     .update({
       umbrellaCount: firebase.firestore.FieldValue.increment(1),
@@ -88,7 +86,7 @@ async function handlePickUp(currentReservation) {
   const { vendorId } = currentReservationDoc.data();
   // decrement umbrella count to vendor
   const decrementUmbrellaCountToVendor = db
-    .collection('vendors')
+    .collection("vendors")
     .doc(vendorId)
     .update({
       umbrellaCount: firebase.firestore.FieldValue.increment(-1),
@@ -108,28 +106,28 @@ async function myUmbrellaMain() {
       console.log(user.uid); // Let's know who the logged-in user is by logging their UID
 
       // get user's info and display name
-      const currentUser = db.collection('users').doc(user.uid);
+      const currentUser = db.collection("users").doc(user.uid);
       const currentUserDoc = await currentUser.get();
       const { name: userFullName, currentReservation: currentReservationId } =
         currentUserDoc.data();
-      document.getElementById('userFullName').innerText = userFullName;
+      document.getElementById("userFullName").innerText = userFullName;
 
       if (currentReservationId) {
         // document.getElementById('no-reservation-message').style.display =
         //   'none';
         // get current reservation info (for 1. calculate remaining time and 2. pickup / return)
         const currentReservation = db
-          .collection('Reservations')
+          .collection("Reservations")
           .doc(currentReservationId);
 
         currentReservation.onSnapshot((doc) => {
           // console.log(doc.data());
           sessionStorage.setItem(
-            'currentReservation',
+            "currentReservation",
             JSON.stringify({
               ...doc.data(),
               id: doc.id,
-            })
+            }),
           );
           const {
             isPickedUp,
@@ -140,44 +138,43 @@ async function myUmbrellaMain() {
           } = doc.data();
           // Display pickup and reserve button
 
-        //   if (!isPickedUp) {
-        //     qrButton.textContent = 'Pick Up';
-        //     qrButton.addEventListener('click', async () => {
-        //         await handlePickUp(currentReservation);
-        //         qrButton.textContent = 'Return'; // Change to 'Return' after pickup
-        //     });
-        // } else {
-        //     qrButton.textContent = 'Return';
-        //     qrButton.addEventListener('click', () => {
-        //         handleReturn(currentUser, currentReservation);
-        //     });
-        // }
+          //   if (!isPickedUp) {
+          //     qrButton.textContent = 'Pick Up';
+          //     qrButton.addEventListener('click', async () => {
+          //         await handlePickUp(currentReservation);
+          //         qrButton.textContent = 'Return'; // Change to 'Return' after pickup
+          //     });
+          // } else {
+          //     qrButton.textContent = 'Return';
+          //     qrButton.addEventListener('click', () => {
+          //         handleReturn(currentUser, currentReservation);
+          //     });
+          // }
 
           if (!isPickedUp) {
             // get reservationTime for timer
             console.log(reservationTime);
             initTimer(reservationTime, false);
             renderVendorCard(vendorId, false);
-            pickUpBtn.style.display = 'block';
-            pickUpBtn.addEventListener('click', (e) => {
+            pickUpBtn.style.display = "block";
+            pickUpBtn.addEventListener("click", (e) => {
               handlePickUp(currentReservation);
             });
             return;
           } else {
-            
             console.log(pickedUpTime);
             initTimer(pickedUpTime, true);
             renderVendorCard(vendorId, true);
-            pickUpBtn.style.display = 'none';
-            returnBtn.style.display = 'block';
-            returnBtn.addEventListener('click', (e) => {
+            pickUpBtn.style.display = "none";
+            returnBtn.style.display = "block";
+            returnBtn.addEventListener("click", (e) => {
               handleReturn(currentUser, currentReservation);
             });
           }
 
           if (isReturned) {
-            pickUpBtn.style.display = 'none';
-            returnBtn.style.display = 'none';
+            pickUpBtn.style.display = "none";
+            returnBtn.style.display = "none";
           }
         });
       } else {
@@ -187,11 +184,11 @@ async function myUmbrellaMain() {
         const noReservationCard =
           '<div class="my-umbrella-no-reservation" id="no-reservation-message"><h1>You Have No Reservation!</h1></div>';
         document
-          .querySelector('.content-container')
-          .insertAdjacentHTML('beforeend', noReservationCard);
+          .querySelector(".content-container")
+          .insertAdjacentHTML("beforeend", noReservationCard);
       }
     } else {
-      throw new Error('No user is logged in.'); // Log a message when no user is logged in
+      throw new Error("No user is logged in."); // Log a message when no user is logged in
     }
     removeLoaderDisplayContent();
   });
@@ -201,28 +198,28 @@ async function myUmbrellaMain() {
 
 function renderModal() {
   // Get the modal
-  var modal = document.getElementById('myModal');
+  var modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementById('myBtn');
+  var btn = document.getElementById("myBtn");
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close')[0];
+  var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal
   btn.onclick = function () {
-    modal.style.display = 'block';
+    modal.style.display = "block";
   };
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.style.display = "none";
     }
   };
 }
