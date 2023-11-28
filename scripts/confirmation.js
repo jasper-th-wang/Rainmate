bbb918f9d1b7084f847d91d0118e50715da85665
Jasper Wang <jasper8777@icloud.com>
2023-11-28 15:22:24 -0800

    Refactor confirmation.js

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let params = new URLSearchParams(document.location.search);
-  let id = params.get("id"); // "id" is the query string parameter
-  if (id) {
-    // Call your function to generate the QR code here, passing the reservation ID
-    generateQRCode(id);
-  } else {
-    // Handle the case where there is no ID in the URL
-    console.log("No reservation ID found.");
-  }
-  removeLoaderDisplayContent();
-});
+/**
+ * Renders QR code based on search parameters
+ */
+function renderQRCodeFromParams() {
+  document.addEventListener("DOMContentLoaded", (event) => {
+    let params = new URLSearchParams(document.location.search);
+    let id = params.get("id"); // "id" is the query string parameter
+    if (id) {
+      // Call your function to generate the QR code here, passing the reservation ID
+      generateQRCode(id);
+    } else {
+      // Handle the case where there is no ID in the URL
+      console.log("No reservation ID found.");
+    }
+    removeLoaderDisplayContent();
+  });
+}
+
+renderQRCodeFromParams();
